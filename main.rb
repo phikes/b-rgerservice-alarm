d6e7f3de24067882289b0f63e32e16a3efdad993
Phillip Kessels <info@phikes.de>
2015-02-23 14:18:12 +0100

    Initial commit.

---
+++ main.rb
@@ -0,0 +1,18 @@
+def beep
+  3.times { `afplay /System/Library/Sounds/Ping.aiff` }
+end
+
+def update
+  res = Net::HTTP.get URI('https://terminmanagement.regioit-aachen.de/aachen/mobile_bahnhofplatz/get.php?option=values')
+  res.scan(/{.*?}/).collect { |r| JSON.parse r}.last.values.first
+end
+
+def alarm(number)
+  loop do
+    sleep 2
+    if update.scan(/\d+/).first.to_i >= number
+      beep
+      return
+    end
+  end
+end
